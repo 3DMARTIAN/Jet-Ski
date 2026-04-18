b7404ace182fe12ab65ed229d4bfd90b954fc8e4
begeritos <72303665+3DMARTIAN@users.noreply.github.com>
2026-04-18 11:18:06 +0700

    Create jetski.py

---
+++ jetski.py
@@ -0,0 +1,11 @@
+def jet_ski_info():
+    facts = [
+        "Jet skis are personal watercraft.",
+        "They can reach high speeds.",
+        "Used for sport and recreation."
+    ]
+    for fact in facts:
+        print(fact)
+
+if __name__ == "__main__":
+    jet_ski_info()
